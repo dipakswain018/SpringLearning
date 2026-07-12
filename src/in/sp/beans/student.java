41d321739c7d7a5c50d2f0d8d40e320569b9d82c
Dipak Kumar Swain <ds1137727@gmail.com>
2026-07-12 22:55:42 +0530

    Initial Spring Project

---
+++ src/in/sp/beans/student.java
@@ -0,0 +1,24 @@
+package in.sp.beans;
+
+public class student {
+	
+	private String name ;
+	private int rollno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public void display() {
+		System.out.println("Name :"+name);
+		System.out.println("marks "+rollno);
+	}
+}
